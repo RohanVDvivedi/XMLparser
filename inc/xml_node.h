04e91be6d1fce72bef2f303ca0c5ee668723bae1
rohan <rohandvivedi@gmail.com>
2021-06-18 20:56:05 +0530

    removed completed task

--- inc/xml_node.h
+++ inc/xml_node.h
@@ -8,24 +8,20 @@
 typedef struct xml_node xml_node;
 struct xml_node
 {
-	dstring tag_name;		// name of the tag i.e. <tag_name> </tag_name> OR <tag_name/>
+	dstring tag_name;		// i.e. <tag_name> ... </tag_name> OR <tag_name/>
 
 	hashmap attributes;		// hashmap of dstring vs dstring
 							// i.e. <tag_name attribute_key="attribute_value" >
 
-	int is_empty : 1;
+	int is_empty : 1;		// an XML tag is empty if
+							// <tag_name></tag_name> OR <tag_name/>
 
-	int has_content : 1;
-
-	// valid only if has_content == 0
-	unsigned int children_count;
+	int has_content : 1;	// or has child_nodes
 
 	// an XML tag contains children_nodes or content
-	// if children_count > 0 then has_content must be 0
-
 	union
 	{
-		array children_nodes;	// a dynamic array of xml_node*
+		linkedlist child_nodes;	// a dynamic array of xml_node*
 
 		dstring content;		// string content
 	}data;
