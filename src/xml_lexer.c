3f00191c2812d36e89a268f44826cf57ed58d64e
rohan <rohandvivedi@gmail.com>
2021-06-26 18:11:12 +0530

    added cases for lexemes and implemented unquoted string case

--- src/xml_lexer.c
+++ src/xml_lexer.c
@@ -29,15 +29,15 @@ int get_next_xml_lexeme(xml_lexer* xml_lexer_p, xml_lexeme* lexeme_p)
 			xml_lexeme_p->value.bytes_occupied = xml_lexer_p->next_token_start - xml_lexeme_p->value.cstring;
 			return 1;
 		}
-		case '<' :
+		case '<' :	// case for < OR </
 		{
 			return 1;
 		}
-		case '/' :
+		case '/' :	// case for />
 		{
 			return 1;
 		}
-		case '>' :
+		case '>' :	// case for >
 		{
 			return 1;
 		}
@@ -55,6 +55,13 @@ int get_next_xml_lexeme(xml_lexer* xml_lexer_p, xml_lexeme* lexeme_p)
 			}
 			else	// case of unquoted string
 			{
+				xml_lexeme_p->type = UNQUOTED_STRING;
+				xml_lexeme_p->value.cstring = xml_lexer_p->next_token_start++;
+				xml_lexeme_p->value.bytes_occupied = 1;
+				while((xml_lexer_p->next_token_start != end_char_at) && !isspace(*(xml_lexer_p->next_token_start))
+					&& (*(xml_lexer_p->next_token_start)) != '<' && (*(xml_lexer_p->next_token_start)) != '>')
+					xml_lexer_p->next_token_start++;
+				xml_lexeme_p->value.bytes_occupied = xml_lexer_p->next_token_start - xml_lexeme_p->value.cstring;
 				return 1;
 			}
 		}
