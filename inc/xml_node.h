2e7f961be26730c00c7ec9677a309894dc415dfa
rohan <rohandvivedi@gmail.com>
2021-06-28 18:45:51 +0530

    added const qualifiers and make empty method

--- inc/xml_node.h
+++ inc/xml_node.h
@@ -41,8 +41,8 @@ struct attribute
 xml_node* get_new_empty_xml_node();
 
 int add_xml_attribute_to_xml_node(xml_node* xml_node_p, const dstring* key, const dstring* value);
-dstring* get_xml_attribute_from_xml_node(xml_node* xml_node_p, const dstring* key);
-int exists_xml_attribute_from_xml_node(xml_node* xml_node_p, const dstring* key);
+dstring* get_xml_attribute_from_xml_node(const xml_node* xml_node_p, const dstring* key);
+int exists_xml_attribute_from_xml_node(const xml_node* xml_node_p, const dstring* key);
 int delete_xml_attribute_from_xml_node(xml_node* xml_node_p, const dstring* key);
 
 int append_content_xml_node(xml_node* xml_node_p, const dstring* content_to_append);
@@ -51,6 +51,8 @@ int clear_content_xml_node(xml_node* xml_node_p);
 int append_child_xml_node(xml_node* xml_node_p, xml_node* child_xml_p);
 xml_node* get_child_xml_node(const xml_node* xml_node_p, unsigned int index);
 int delete_child_xml_node(xml_node* xml_node_p, unsigned int index);
+
+int make_empty_xml_node(xml_node* xml_node_p);
 
 void delete_xml_node_tree(xml_node* xml_node_p);
 
